1b820817aa364878f8f28f00adb0daefe56c50fe
Shalev <admin@shalev.eu.org>
2025-12-04 17:55:51 +0800

    Delete .vercel/output directory

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter__gOFsw0V.mjs';
--- .vercel/output/_functions/pages/api/register.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../chunks/register_CV4jLPda.mjs';
-export { renderers } from '../../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
